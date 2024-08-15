48b2ef18eb6d8e0814b98f4bcc88b357e44e3cfa
aciosui <mrana.swe@gmail.com>
2024-08-15 09:20:02 -0400

    vuex basic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <base-container title="Vuex" v-if="isAuth">
+  <display-counter></display-counter>
+  <FavoriteValue></FavoriteValue>
+  <button @click="addOne">Add 10</button>
+  <ChangeCounter></ChangeCounter>
+  </base-container>
+  <base-container title="Auth">
+  <user-auth></user-auth>
+  </base-container>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseContainer from './components/BaseContainer.vue';
+import DisplayCounter from './components/DisplayCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue'
+import FavoriteValue from './components/FavoriteValue.vue'
+import UserAuth from './components/UserAuth.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    BaseContainer,
+    DisplayCounter,
+    ChangeCounter,
+    FavoriteValue,
+    UserAuth
+  },
+  computed: {
+        isAuth() {
+           return this.$store.getters.userAuthenticated;
+        }
+    },
+
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', {value: 10 })
+      this.$store.dispatch({
+        type: 'increase',
+        value: 10
+      })
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,88 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
 
-createApp(App).mount('#app')
+import { createStore } from 'vuex';
+
+import App from './App.vue';
+
+const counterModule = {
+    state() {
+        return {
+            counter: 0,
+        }
+    },
+    mutations: {
+        increment(state) {
+            state.counter = state.counter + 2
+        },
+        increase(state, payload) {
+            state.counter = state.counter + payload.value;
+        },
+    },
+    actions: {
+        increment(context) {
+            setTimeout(function() {
+                context.commit('increment')
+            }, 2000)
+        },
+        increase(context, payload) {
+            context.commit('increase', payload)
+        },
+    },
+    getters: {
+        finalCounter(state) {
+            return state.counter * 2
+        },
+        normalizedCounter(_, getters) {
+            const finalCounter = getters.finalCounter;
+            if (finalCounter < 0) {
+                return 0;
+            }
+            if(finalCounter > 100) {
+                return 100;
+            }
+            return finalCounter
+        },
+    }
+};
+
+const store = createStore({
+    modules: {
+        numbers: counterModule
+    },
+    state() {
+        return{
+            
+            isLoggedIn: false
+        }
+    },
+    // state change in method
+    mutations: {
+  
+        setAuth(state, payload) {
+            state.isLoggedIn = payload.isAuth
+        }
+    },
+    // async action 
+    actions: {
+
+        login(context) {
+            context.commit('setAuth', {isAuth: true})
+        },
+        logout(context) {
+            context.commit('setAuth', {isAuth: false})
+        }
+    },
+    // computed value
+    getters: {
+ 
+        userAuthenticated(state) {
+            return state.isLoggedIn;
+        }
+    }
+})
+
+const app = createApp(App);
+
+app.use(store)
+
+app.mount('#app');
